34d0d506c0ccff2c261ab51650c0909900fc1932
parv8055 <choudharypar4@gmail.com>
2024-03-22 09:53:45 +0530

    major changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import { Badge } from "@/components/ui/badge";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
-import { Chip, Progress } from "@nextui-org/react";
+import { Chip, Progress, Snippet } from "@nextui-org/react";
 import { Button } from "@/components/ui/button";
 import { FaFacebookSquare, FaInstagram, FaRegHeart } from "react-icons/fa";
 import { FiTwitter } from "react-icons/fi";
 import { BsGlobe } from "react-icons/bs";
 import { RiDiscordLine } from "react-icons/ri";
 import { ReactNode } from "react";
+import * as CurrencyFormat from "react-currency-format";
+import DateDisplay from "@/components/DateDisplay";
 
 function Player() {
   return (
@@ -25,11 +27,22 @@ function Player() {
 
 function DetailsComp({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 justify-between">
+    <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:justify-between">
       {children}
     </div>
   );
 }
+
+function formatDate(date: Date): string {
+  return `${date.getFullYear()}.${
+    date.getMonth() + 1
+  }.${date.getDate()} ${date.getHours()}:${date.getMinutes()} (UTC)`;
+}
+
+const dateStart = new Date();
+const dateEnd = new Date();
+dateEnd.setTime(dateEnd.getTime() + 10 * 24 * 60 * 60 * 1000);
+
 export default function Home() {
   const data = {
     PresaleAddress: "0x01ab470f6016632acf9b8234d8a53175a2c35329",
@@ -56,12 +69,14 @@ export default function Home() {
                 width={100}
                 height={100}
                 alt="Picture of the author"
-                className="rounded-full object-cover"
+                className="rounded-full object-cover mx-auto"
               />
             </div>
             <div className="w-10/12 space-y-4">
-              <div className="flex space-x-4">
-                <span>Pyramid Fair Launch</span>
+              <div className="flex space-x-2 md:space-x-4">
+                <span className="text-xl md:text-3xl font-extrabold">
+                  Pyramid Fair Launch
+                </span>
                 <Chip color="primary" variant="faded">
                   <FaRegHeart />
                 </Chip>
@@ -111,80 +126,85 @@ export default function Home() {
                 voluptas blanditiis eligendi amet corporis, vero saepe obcaecati
                 voluptatum unde quidem cum exercitationem quae.
               </p>
-              <div className="w-full max-w-5xl mx-auto hidden md:block">
+              <div className="w-full max-w-2xl mx-auto hidden md:block">
                 <Player />
               </div>
             </div>
           </div>
-          <div className="w-full max-w-5xl mx-auto mt-4 block md:hidden">
+          <div className="w-full mx-auto mt-4 block md:hidden">
             <Player />
           </div>
           <div className="mt-12">
             <DetailsComp>
-              <span>Presale Address :</span>
-              <h1>{data.PresaleAddress}</h1>
+              <span className="text-lg font-bold">Presale Address :</span>
+              <Snippet hideSymbol={true} variant="shadow" color="secondary">
+                {data.PresaleAddress}
+              </Snippet>
             </DetailsComp>
             <hr className="my-4" />
             <DetailsComp>
-              <span>Token Name :</span>
+              <span className="text-lg font-bold">Token Name :</span>
               <span>{data.TokenName}</span>
             </DetailsComp>
             <hr className="my-4" />
-            <div className="flex flex-col sm:flex-row justify-between">
-              <span>Token Symbol :</span>
+            <DetailsComp>
+              <span className="text-lg font-bold">Token Symbol :</span>
               <span>{data.TokenSymbol}</span>
-            </div>
+            </DetailsComp>
             <hr className="my-4" />
-            <div className="flex flex-col sm:flex-row justify-between">
-              <span>Token Decimals :</span>
+            <DetailsComp>
+              <span className="text-lg font-bold">Token Decimals :</span>
               <span>{data.TokenDecimals}</span>
-            </div>
+            </DetailsComp>
             <hr className="my-4" />
-            <div className="flex flex-col sm:flex-row justify-between">
-              <span>Token Address :</span>
-              <span>{data.TokenAddress}</span>
-            </div>
+            <DetailsComp>
+              <span className="text-lg font-bold">Token Address :</span>
+              <Snippet hideSymbol={true} variant="shadow" color="secondary">
+                {data.TokenAddress}
+              </Snippet>
+            </DetailsComp>
             <hr className="my-4" />
-            <div className="flex flex-col sm:flex-row justify-between">
-              <span>Total Supply :</span>
+            <DetailsComp>
+              <span className="text-lg font-bold">Total Supply :</span>
               <span>
-                {data.TotalSupply} {data.TokenName}
+                {data.TotalSupply.toLocaleString("en-US")} {data.TokenName}
               </span>
-            </div>
+            </DetailsComp>
             <hr className="my-4" />
-            <div className="flex flex-col sm:flex-row justify-between">
-              <span>Tokens For Presale :</span>
+            <DetailsComp>
+              <span className="text-lg font-bold">Tokens For Presale :</span>
               <span>
-                {data.TokensForPresale} {data.TokenName}
+                {data.TokensForPresale.toLocaleString("en-US")} {data.TokenName}
               </span>
-            </div>
+            </DetailsComp>
             <hr className="my-4" />
-            <div className="flex flex-col sm:flex-row justify-between">
-              <span>Tokens For Liquidity :</span>
+            <DetailsComp>
+              <span className="text-lg font-bold">Tokens For Liquidity :</span>
               <span>
-                {data.TokensForLiquidity} {data.TokenName}
+                {data.TokensForLiquidity.toLocaleString("en-US")}{" "}
+                {data.TokenName}
               </span>
-            </div>
+            </DetailsComp>
             <hr className="my-4" />
-            <div className="flex flex-col sm:flex-row justify-between">
-              <span>Initial Market Cap :</span>
-              <span>$ {data.InitialMarketCap}</span>
-            </div>
+            <DetailsComp>
+              <span className="text-lg font-bold">Initial Market Cap :</span>
+              <span>$ {data.InitialMarketCap.toLocaleString("en-US")}</span>
+            </DetailsComp>
             <hr className="my-4" />
-            <div className="flex flex-col sm:flex-row justify-between">
-              <span>Soft Cap :</span>
+            <DetailsComp>
+              <span className="text-lg font-bold">Soft Cap :</span>
               <span>{data.SoftCap} BNB</span>
-            </div>
+            </DetailsComp>
             <hr className="my-4" />
-            <div className="flex flex-col sm:flex-row justify-between">
-              <span>Presale Start Time :</span>
-              <span>{data.PresaleStartTime}</span>
-            </div>
+            <DetailsComp>
+              <span className="text-lg font-bold">Presale Start Time :</span>
+              <span>{formatDate(dateStart)}</span>
+            </DetailsComp>
             <hr className="my-4" />
-            <div className="flex flex-col sm:flex-row justify-between">
-              <span>Presale End Time :</span>
-              <span>{data.PresaleEndTime}</span>
-            </div>
+            <DetailsComp>
+              <span className="text-lg font-bold">Presale End Time :</span>
+              <span>{formatDate(dateEnd)}</span>
+            </DetailsComp>
             <hr className="my-4" />
           </div>
         </Card>
@@ -195,7 +215,7 @@ export default function Home() {
             size="lg"
             value={70}
           />
-          <div className="flex justify-between">
+          <div className="flex justify-between font-bold">
             <div className="flex flex-col text-left">
               <span>55</span>
               <span>BNB</span>
@@ -205,7 +225,7 @@ export default function Home() {
               <span>BNB</span>
             </div>
           </div>
-          <h1 className="text-center">This pool is Closed</h1>
+          <h1 className="text-center font-bold">This pool is Closed</h1>
           <Button variant="link" className="underline block mx-auto">
             Veiw Chart
           </Button>
